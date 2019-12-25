0d04f61876a2d11ea34418ed5d1b15100e68657e
Gero <geropellicer@gmail.comm>
2019-12-25 15:18:53 -0300

    Registered  planes models to admin site

--- backend/adminPts/planes/admin.py
+++ backend/adminPts/planes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from planes.models import estadoPersonaPlan, personaPlan, plan
 
 # Register your models here.
+admin.site.register(estadoPersonaPlan)
+admin.site.register(personaPlan)
+admin.site.register(plan)
