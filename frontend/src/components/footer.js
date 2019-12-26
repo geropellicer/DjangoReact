d2a53e1a3c951607e769d4aced31ea0e2072f855
Gero <geropellicer@gmail.comm>
2019-12-26 10:09:38 -0300

    Added basic Footer and Header structure

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer className="mainFooter">
+            <h2>Admin PTS</h2>
+
+            <nav>
+                <ul>
+                    <li>
+                        Un item
+                    </li>
+                    <li>
+                        Otro item
+                    </li>
+                </ul>
+            </nav>
+
+            <h3>Actualizado al 2020</h3>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/components/nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Nav = () => {
+    return(
+        <header className="mainHeader">
+            <h2>Admin PTS</h2>
+
+            <nav>
+                <ul>
+                    <li>
+                        Un item
+                    </li>
+                    <li>
+                        Otro item
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Nav;
