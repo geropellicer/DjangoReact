da2986cc5c11b4320265aa79bfdff30294743b4c
Gero <geropellicer@gmail.comm>
2019-12-26 10:08:05 -0300

    Added router basics and basic structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Nav from './components/nav';
+import Footer from './components/footer';
+
+import Home from './views/home';
+import Estructuras from './views/estructuras';
+import Estructura from './views/estructura';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Router>
+        <Nav/>
+        <div className="appContent">
+          <Switch>
+            <Route path="/" component={Home} exact/>
+            <Route path="/estructuras" component={Estructuras} exact/>
+            <Route path="/estructuras/:id" component={Estructura} exact/>
+          </Switch>
+        </div>
+        <Footer/>
+      </Router>
     </div>
   );
 }
