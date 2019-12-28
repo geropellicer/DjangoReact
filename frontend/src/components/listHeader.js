67962ca98389cb40b3be441e9d5a882c1d486c8f
Gero <geropellicer@gmail.comm>
2019-12-28 20:40:14 -0300

    Added edit and remove buttons to ListHeader

--- frontend/src/components/listHeader.js
+++ frontend/src/components/listHeader.js
@@ -6,11 +6,23 @@ const ListHeader = ({titulo, url}) => {
     return(
         <header className="sectionHeader fila">
             <h3>{titulo}</h3>
-            <Link to={url}>
-                <button className="fila">
-                    Agregar <ion-icon name="add-circle-outline"></ion-icon>
-                </button>
-            </Link>
+            <div className="fila">
+                <Link to={'/'}>
+                    <button className="fila eliminar">
+                        <ion-icon name="trash"></ion-icon>
+                    </button>
+                </Link>
+                <Link to={'/'}>
+                    <button className="fila editar">
+                        <ion-icon name="create"></ion-icon>
+                    </button>
+                </Link>
+                <Link to={url}>
+                    <button className="fila agregar">
+                        Agregar <ion-icon name="add-circle-outline"></ion-icon>
+                    </button>
+                </Link>
+            </div>
         </header>
     );
 };
