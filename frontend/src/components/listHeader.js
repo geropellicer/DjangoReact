94ec6d5bbed71df5f95f64fefc7338aa18becc66
Gero <geropellicer@gmail.comm>
2019-12-28 17:47:40 -0300

    Added router funciontality to add button

--- frontend/src/components/listHeader.js
+++ frontend/src/components/listHeader.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const ListHeader = ({titulo}) => {
+const ListHeader = ({titulo, url}) => {
 
     return(
         <header className="sectionHeader fila">
             <h3>{titulo}</h3>
-            <button className="fila">
-                Agregar <ion-icon name="add-circle-outline"></ion-icon>
-            </button>
+            <Link to={url}>
+                <button className="fila">
+                    Agregar <ion-icon name="add-circle-outline"></ion-icon>
+                </button>
+            </Link>
         </header>
     );
 };
