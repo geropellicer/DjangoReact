18eea5fcf874ad957c020f750eeb9a74e1ca5e8d
Gero <geropellicer@gmail.comm>
2019-12-30 13:52:24 -0300

    Added auth reducer for logging in

--- frontend/src/redux/reducers/auth.js
+++ frontend/src/redux/reducers/auth.js
@@ -1,12 +1,53 @@
+import {userLoading, userLoaded, authError} from '../actions'
+import {
+    GET_ERRORS,
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGOUT
+} from '../actions/types';
+import apiService from '../../config/apiService';
 const initialState = {
     token: localStorage.getItem('authToken'),
-    isAuthenticated: null,
+    isAuthenticated: false,
     isLoading: false,
     user: null
 }
 
 const authReducer = (state = initialState, action) => {
     switch(state){
+        case USER_LOADING:
+            return{
+                ...state,
+                isLoading: true
+            }
+        case USER_LOADED:
+            return{
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+            }
+        case AUTH_ERROR:
+            localStorage.removeItem('authToken');
+            return{
+                ...state,
+                token: null,
+                user: null,
+                isAuthenticated: false,
+                isLoading: false
+            }
+        case LOGOUT:
+            console.log('sucediendo');
+            localStorage.removeItem('authToken');
+            apiService('rest-auth/logout/', 'POST');
+            return{
+                ...state,
+                token: null,
+                user: null,
+                isAuthenticated: false,
+                isLoading: false
+            }
         default:
             return state;
     }
