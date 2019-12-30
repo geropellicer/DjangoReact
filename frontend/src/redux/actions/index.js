92164d93e2cbdaefe6072ec048d5c170448ef8f3
Gero <geropellicer@gmail.comm>
2019-12-30 13:51:52 -0300

    Added actions types

--- frontend/src/redux/actions/index.js
+++ frontend/src/redux/actions/index.js
@@ -1,35 +1,80 @@
+import {
+    GET_ERRORS,
+    ACTIVATE_EDIT,
+    DEACTIVATE_EDIT,
+    TOGGLE_EDIT,
+    ACTIVATE_REMOVE,
+    DEACTIVATE_REMOVE,
+    TOGGLE_REMOVE,
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGOUT
+} from './types';
+
+export const returnErrors = (msg, status) => {
+    return{
+        type: GET_ERRORS,
+        payload: {msg, status}
+    }
+}
+
 export const activateEdit = () => {
     return{
-        type: 'ACTIVATEEDIT'
+        type: ACTIVATE_EDIT
     };
 };
 
 export const deactivateEdit = () => {
     return{
-        type: 'DEACTIVATEEDIT'
+        type: DEACTIVATE_EDIT
     };
 };
 
 export const toggleEdit = () => {
     return{
-        type: 'TOGGLEEDIT'
+        type: TOGGLE_EDIT
     };
 };
 
 export const activateRemove = () => {
     return{
-        type: 'ACTIVATEREMOVE'
+        type: ACTIVATE_REMOVE
     };
 };
 
 export const deactivateRemove = () => {
     return{
-        type: 'DEACTIVATEREMOVE'
+        type: DEACTIVATE_REMOVE
     };
 };
 
 export const toggleRemove = () => {
     return{
-        type: 'TOGGLEREMOVE'
+        type: TOGGLE_REMOVE
     };
 };
+
+export const userLoading = () => {
+    return {
+        type: USER_LOADING
+    }
+}
+
+export const userLoaded = () => {
+    return {
+        type: USER_LOADED
+    }
+}
+
+export const authError = () => {
+    return {
+        type: AUTH_ERROR
+    }
+}
+
+export const logout = () => {
+    return {
+        type: LOGOUT
+    }
+}
